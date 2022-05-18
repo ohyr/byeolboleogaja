1b41fc2b9a4eddc34a48d37df15b378bec64241a
zlflshdh84 <zlflshdh84@gmail.com>
2022-05-18 11:08:07 +0900

    Feat: add footsteps sound to menu
    Former-commit-id: e85a420fb5bb7539cb52589744d155e4515e60b3 [formerly e96e10148f4bdbc45ce101563ca4f3ba0f1f7112]
    Former-commit-id: e7cde2ddb2cfe7ed54929873dfa7974c0258bf61

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,29 +1,51 @@
 import { SpeedDial, SpeedDialIcon, SpeedDialAction } from '@mui/material';
-import { Logout, VolumeUp, Public, QuestionMark } from '@mui/icons-material';
+import { Logout, VolumeUp, Public, QuestionMark, DoNotStep } from '@mui/icons-material';
 import SoundDialog from '@/components/SoundDialog';
 
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { bgmInfo } from '@/constants';
+
 const Menu = ({ isGuideDialog, isWorld, setGuideOpen, placeBGM }) => {
   const navigate = useNavigate();
 
-  const [value, setValue] = useState(0.2);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const audioSetting = document.querySelector('audio');
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0.2);
+
+  const [anchor1El, setAnchor1El] = useState(null);
+  const [anchor2El, setAnchor2El] = useState(null);
+
+  const audioSetting = document.querySelector('#soundVolume');
+  const footstepsSetting = document.querySelector('#footsteps');
   useEffect(() => {
-    const audioSetting = document.querySelector('audio');
+    const audioSetting = document.querySelector('#soundVolume');
     const nowPlaceBGM = bgmInfo.filter((item, idx) => item.place == placeBGM);
     if (Object.keys(nowPlaceBGM).length != 0) {
       audioSetting.src = nowPlaceBGM[0].bgm;
-      audioSetting.volume = 0.2;
+      audioSetting.volume = 0;
     }
+
+    const footstepsSetting = document.querySelector('#footsteps');
+    const footstepsBGM = bgmInfo.filter((item, idx) => item.place == 'everywhere');
+    if (Object.keys(footstepsBGM).length != 0) {
+      footstepsSetting.src = footstepsBGM[0].bgm;
+      footstepsSetting.volume = 0.2;
+    }
+
+    window.addEventListener('keydown', function (e) {
+      if (e.key == 'w' || e.key == 'a' || e.key == 's' || e.key == 'd') {
+        footstepsSetting.play();
+      }
+    });
   }, []);
   const goMain = () => {
     navigate('/');
   };
   const controlSounds = (event) => {
-    setAnchorEl(event.currentTarget);
+    setAnchor1El(event.currentTarget);
+  };
+  const controlSounds2 = (event) => {
+    setAnchor2El(event.currentTarget);
   };
   const goWorld = () => {
     navigate('/world');
@@ -36,7 +58,8 @@ const Menu = ({ isGuideDialog, isWorld, setGuideOpen, placeBGM }) => {
   const actionMenu = [
     { icon: <Logout />, name: '종료하기', move: goMain },
     { icon: <VolumeUp />, name: '배경음악 조절', move: controlSounds },
-
+    { icon: <DoNotStep />, name: '발소리 조절', move: controlSounds2 },
+    
     isWorld ? { icon: <Public />, name: '월드로 돌아가기', move: goWorld } : {},
     isGuideDialog
       ? {
@@ -46,7 +69,7 @@ const Menu = ({ isGuideDialog, isWorld, setGuideOpen, placeBGM }) => {
         }
       : {},
   ];
-  const NewactionMenu = actionMenu.filter(
+  const newActionMenu = actionMenu.filter(
     (element, i) => Object.keys(element).length != 0,
   );
 
@@ -55,16 +78,21 @@ const Menu = ({ isGuideDialog, isWorld, setGuideOpen, placeBGM }) => {
       <audio
         id="soundVolume"
         src="#"
-        volume={value}
+        volume={value1}
         autoPlay={true}
         loop={true}
+      ></audio>
+      <audio
+        id="footsteps"
+        src="#"
+        volume={value2}
       ></audio>
       <SpeedDial
         ariaLabel="Museum menu"
         sx={{ position: 'absolute', bottom: 16, right: 16 }}
         icon={<SpeedDialIcon />}
       >
-        {NewactionMenu.map((action) => (
+        {newActionMenu.map((action) => (
           <SpeedDialAction
             key={action.name}
             icon={action.icon}
@@ -74,11 +102,18 @@ const Menu = ({ isGuideDialog, isWorld, setGuideOpen, placeBGM }) => {
         ))}
       </SpeedDial>
       <SoundDialog
-        anchorEl={anchorEl}
-        setAnchorEl={setAnchorEl}
-        value={value}
-        setValue={setValue}
+        anchorEl={anchor1El}
+        setAnchorEl={setAnchor1El}
+        value={value1}
+        setValue={setValue1}
         audioSetting={audioSetting}
+      />
+      <SoundDialog
+        anchorEl={anchor2El}
+        setAnchorEl={setAnchor2El}
+        value={value2}
+        setValue={setValue2}
+        audioSetting={footstepsSetting}
       />
     </>
   );
